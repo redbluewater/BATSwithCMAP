df8108b57953a5764d72293d689202f26164f37a
Krista Longnecker <klongnecker@whoi.edu>
2025-06-16 14:17:27 -0700

    working on slurm

--- matchToBATS/config/config.py
+++ matchToBATS/config/config.py
@@ -2,4 +2,5 @@
 ## Register at https://simonscmap.com/ and get an API_KEY.
 ## Plug in your API_KEY below.
 
-API_KEY = "8147c3f6-e99c-47f5-9d72-1290ae601ebb"
+#KL make anonymous for now
+API_KEY = <PUT KEY HERE>
