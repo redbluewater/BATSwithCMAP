63bd95f90f67da681898cb3df45f6ea5d4199e23
Krista Longnecker <klongnecker@poseidon-l2.cm.cluster>
2025-06-18 12:54:56 -0400

    troubleshooting

--- matchToBATS/config/config.py
+++ matchToBATS/config/config.py
@@ -2,5 +2,5 @@
 ## Register at https://simonscmap.com/ and get an API_KEY.
 ## Plug in your API_KEY below.
 
-#KL make anonymous for now
-API_KEY = <PUT KEY HERE>
+# this is in .gitignore, but need the key on Poseidon
+API_KEY = "8147c3f6-e99c-47f5-9d72-1290ae601ebb"
