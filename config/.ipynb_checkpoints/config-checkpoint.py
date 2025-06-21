ab525304c4227252fd86705685a877977fc907ab
Krista Longnecker <klongnecker@whoi.edu>
2025-06-21 16:07:22 -0700

    finetuning

--- config/.ipynb_checkpoints/config-checkpoint.py
+++
@@ -1,5 +0,0 @@
-## Register at https://simonscmap.com/ and get an API_KEY.
-## Plug in your API_KEY below.
-
-#KL make sure this is in a place with .gitignore
-API_KEY = '8147c3f6-e99c-47f5-9d72-1290ae601ebb'
